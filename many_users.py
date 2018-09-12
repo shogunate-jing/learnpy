ada1c0c0bc59efa4bd05309c1ef4e0ffb6f50553
fj <shogunate@live.com>
2018-09-12 22:37:12 +0800

    many users

---
+++ many_users.py
@@ -0,0 +1,23 @@
+users = {
+    'aeinstein':{
+        'first':'albert',
+        'last':'einstein',
+        'location':'princeton',
+        },
+    
+    'mcurie':{
+        'first':'marie',
+        'last':'curie',
+        'location':'paris',
+        },
+    }
+
+for username,user_info in users.items():
+    print("\nUsername :" + username)
+    fullname = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    print("\t Full name: " + fullname.title())
+    print("\t Location: " + location.title())
+
+message = print("tell me someting")
+print(message)
